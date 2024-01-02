aa5177678c95037c6edf02bb389a19e0d46f3038
kubat <kubat.aiupov@gmail.com>
2024-01-02 12:44:56 +0100

    def pages, create routes and links

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
 import './App.scss'
 import Home from './pages/Home';
 import About from './pages/About';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Privacy from './pages/Privacy';
+import Terms from './pages/Terms';
+import Write from './pages/Write';
+import Contact from './pages/Contact';
+import Login from './pages/Login';
+import Post from './pages/Post';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <div>
-        <Link to='/'>Home</Link>
-        <Link to='/about'>About</Link>
+        <Navbar />
         <Routes>
           <Route path='/about' element={<About />}>About</Route>
-          <Route path='/' element={<Home />}>Home</Route>
+          <Route path='/privacy' element={<Privacy />}>Privacy</Route>
+          <Route path='/terms' element={<Terms />}>Terms</Route>
+          <Route path='/write' element={<Write />}>Write</Route>
+          <Route path='/contact' element={<Contact />}>Contact</Route>
+          <Route path='/login' element={<Login />}>Login</Route>
+          <Route path='/' element={<Home />}>
+            <Route path='/' element={<Home />}>Home</Route>
+            <Route path='/:id' element={<Post />}>Post</Route>
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
