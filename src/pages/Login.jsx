c0d5f8bc70fe3852d5092287a684d9249723f58c
kubat <kubat.aiupov@gmail.com>
2024-01-05 11:42:20 +0100

    init login page and style for it

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Login.scss";
 
 export default function Login() {
   return (
-    <div>Login Page goes here!</div>
-  )
+    <div className="login-container">
+      <h2 className="login-title">Log/In</h2>
+      <form className="login-form">
+        <label htmlFor="login-email" className="login-label">
+          Email
+        </label>
+        <input
+          type="email"
+          id="login-email"
+          className="login-input"
+          placeholder="userName@mail.com"
+        />
+        <label htmlFor="login-user-password" className="login-label">
+          Password
+        </label>
+        <input
+          type="password"
+          id="login-user-password"
+          className="login-input"
+          placeholder="enter your password"
+        />
+        <div className="login-cta">
+          <button type="submit" className="login-btn">
+            Login
+          </button>
+          <p>or</p>
+          <Link to="signup" className="login-btn">
+            Sign Up
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
 }
