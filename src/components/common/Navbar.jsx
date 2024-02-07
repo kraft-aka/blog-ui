7d51b7ff00bba382ff41a4569befc964f8b37816
kubat <kubat.aiupov@gmail.com>
2024-02-07 11:36:08 +0100

    add Logout btn, add style to btn

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.svg";
-import user from '../../assets/user.svg';
+import user from "../../assets/user.svg";
 import "./Navbar.scss";
 import { useAuth } from "../../providers/authProvider";
 
 export default function Navbar() {
-  const {token} = useAuth();
+  const { token } = useAuth();
   return (
     <nav className="navbar">
       <Link to="/">
         <img src={logo} alt="logo" className="navbar-logo" />
       </Link>
       <div className="navbar-cta">
-        { token && <Link to='/user' className="navbar-user"><img src={user} alt="default user icon" /></Link>}
+        {token && (
+          <Link to="/user" className="navbar-user">
+            <img src={user} alt="default user icon" />
+          </Link>
+        )}
+        {token && <button className="navbar-btn">Log Out</button>}
         <Link to="/signin" className="navbar-link">
           Sign In
         </Link>
