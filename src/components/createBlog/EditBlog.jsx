fa24720667a765abb9585550ed5f75f00adacadc
kubat <kubat.aiupov@gmail.com>
2024-02-25 12:56:04 +0100

    add img update to EditBlog

--- src/components/createBlog/EditBlog.jsx
+++ src/components/createBlog/EditBlog.jsx
@@ -1,54 +1,85 @@
-import React, { useState } from 'react'
-import './EditBlog.scss'
-import { editBlog } from '../../API/blogs';
-import toast from 'react-hot-toast';
+import React, { useState } from "react";
+import "./EditBlog.scss";
+import { editBlog, addImage } from "../../API/blogs";
+import toast from "react-hot-toast";
 
 export default function EditBlog({ handleEditClose, blogs }) {
   const [editTitle, setEditTitle] = useState(blogs.title);
-  const [editContent, setEditContent] = useState(blogs.blogContent)
-  const [updatedBlog, setUpdatedBlog] = useState(null)
+  const [editContent, setEditContent] = useState(blogs.blogContent);
+  const [updatedBlog, setUpdatedBlog] = useState(null);
+  const [img, setImg] = useState(null);
 
   function handleTitleChange(e) {
-    setEditTitle(e.target.value)
+    setEditTitle(e.target.value);
   }
 
   function handleContentChange(e) {
-    setEditContent(e.target.value)
+    setEditContent(e.target.value);
   }
+
+  const handleImageChange = (e) => {
+    setImg(e.target.files[0]);
+  };
 
   const editedBlog = {
     title: editTitle,
     blogContent: editContent,
-  }
+  };
 
-  function handleSubmitEditBlog(e) {
+  async function handleSubmitEditBlog(e) {
     e.preventDefault();
-    editBlog(blogs._id, editedBlog).then(response => {
-      setUpdatedBlog(response.data.blog)
-      toast.success('Blog has been successfully updated!')
-      handleEditClose()
-    }).catch(error => toast.error(error))
+    setUpdatedBlog(false)
+    try {
+      const blog = await editBlog(blogs._id, editedBlog);
+      if (blog) {
+        const blogId = blog.data.blog._id;
+        if (img) {
+          const formData = new FormData();
+          formData.append("uploadFile", img);
+          await addImage(blogId, formData);
+        }
+        setUpdatedBlog(true)
+        toast.success("Blog has been successfully updated!");
+        handleEditClose();
+      } else {
+        toast.error("Failed to update the Blog.");
+      }
+    } catch (err) {
+      toast.error(err.message);
+    }
   }
 
   return (
     <main className="edit-blog-modal">
-      <section className='edit-blog-container'>
-        <header className='edit-blog-header'>
+      <section className="edit-blog-container">
+        <header className="edit-blog-header">
           <h3>Edit Your Blog</h3>
         </header>
-        <form className='edit-blog-form' onSubmit={handleSubmitEditBlog}>
+        <form className="edit-blog-form" onSubmit={handleSubmitEditBlog}>
           <label htmlFor="edit-blog-title">Title</label>
           <input type="text" value={editTitle} onChange={handleTitleChange} />
           <br />
           <label htmlFor="edit-blog-content">Content</label>
           <textarea value={editContent} onChange={handleContentChange} />
           <br />
+          <label htmlFor="image">Image</label>
+          <input
+            type="file"
+            className="blog-edit-img"
+            id="image"
+            name="image"
+            onChange={handleImageChange}
+          />
           <div className="edit-blog-cta">
-            <input type='submit' className='edit-blog-btn' />
-            <input type='reset' onClick={handleEditClose} className='edit-blog-btn cancel' />
+            <input type="submit" className="edit-blog-btn" />
+            <input
+              type="reset"
+              onClick={handleEditClose}
+              className="edit-blog-btn cancel"
+            />
           </div>
         </form>
       </section>
     </main>
-  )
+  );
 }
