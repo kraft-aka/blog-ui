df939ea59bfd1ee7907f33593ea068352931d4de
kubat <kubat.aiupov@gmail.com>
2024-03-17 13:23:22 +0100

    add show username on hover over user icon in navbar

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.svg";
 import user from "../../assets/user.svg";
@@ -7,18 +7,41 @@ import { useAuth } from "../../providers/authProvider";
 
 export default function Navbar() {
   const { token, loggedUser, Logout } = useAuth();
+  const [hover, setHover] = useState(false);
+
+  function onHover() {
+    setHover(true);
+  }
+
+  function leaveHover() {
+    setHover(false);
+  }
+
   return (
     <nav className="navbar">
       <Link to="/">
         <img src={logo} alt="logo" className="navbar-logo" />
       </Link>
-      <div className="navbar-cta">
+      <div
+        className="navbar-cta"
+        onMouseEnter={onHover}
+        onMouseLeave={leaveHover}
+      >
         {token && (
           <Link to="/user" className="navbar-user">
             <img src={user} alt="default user icon" />
+            {hover && (
+              <span className="navbar-display-username">
+                {loggedUser.userName}
+              </span>
+            )}
           </Link>
         )}
-        {token && <button className="navbar-btn" onClick={Logout}>Log Out</button>}
+        {token && (
+          <button className="navbar-btn" onClick={Logout}>
+            Log Out
+          </button>
+        )}
         {!loggedUser && (
           <Link to="/signin" className="navbar-link">
             Sign In
