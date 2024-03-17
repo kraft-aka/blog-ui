bcaa87e51b98979d1db680de390d3e549b44e6c6
kubat <kubat.aiupov@gmail.com>
2024-03-17 12:11:16 +0100

    add implement edit user password

--- src/components/userBoard/UserPassword.jsx
+++ src/components/userBoard/UserPassword.jsx
@@ -1,29 +1,74 @@
-import React, { useState, useEffect } from 'react'
-import { useAuth } from '../../providers/authProvider';
-import './UserCard.scss'
+import React, { useState } from "react";
+import { inputValueIsValid } from "../../utils/inputValueIsValid";
+import { editUserPassword } from "../../API/users";
+import toast from "react-hot-toast";
+import LoadingSpinner from "../UI/LoadingSpinner";
+import "./UserCard.scss";
 
 export default function UserPassword() {
-  const { loggedUser } = useAuth();
-  const [newPassword, setNewPassword] = useState('');
+  const [newPassword, setNewPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState(false);
+
+  // style of error message
+  const errorMsgStyle = {
+    color: "red",
+    fontSize: ".8rem",
+    marginBottom: ".5rem",
+  };
 
   // submit handler
-  function handleEditUserPasswordSubmit(e) {
+  async function handleEditUserPasswordSubmit(e) {
     e.preventDefault();
-  
+    if (!inputValueIsValid(newPassword)) {
+      setErrorMsg(true);
+      toast.error("Password value is not correct.");
+      return;
+    }
+
+    try {
+      setIsLoading(true);
+      const editedPassword = {
+        password: newPassword,
+      };
+      await editUserPassword(editedPassword);
+      setIsLoading(false);
+      setNewPassword("");
+      toast.success("Successfully edited password!");
+    } catch (error) {
+      toast.error("Error occured!");
+      setIsLoading(false);
+    } finally {
+      setErrorMsg(false)
+    }
   }
 
   return (
-    <form className="user-card-update-form" onSubmit={handleEditUserPasswordSubmit}>
-      <label htmlFor="password">Password</label>
-      <input
-        type="password"
-        placeholder="********"
-        value={newPassword}
-        onChange={(e) => setNewPassword(e.target.value)}
-      />
-      <button className="user-card-update-btn" type="submit">
-        Save
-      </button>
-    </form>
-  )
+    <>
+      {!isLoading ? (
+        <form
+          className="user-card-update-form"
+          onSubmit={handleEditUserPasswordSubmit}
+        >
+          <label htmlFor="password">Password</label>
+          {errorMsg && (
+            <p style={errorMsgStyle}>
+              Please type new password. Empty password field is not acceptable!
+            </p>
+          )}
+          <input
+            type="password"
+            placeholder="********"
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
+          />
+          <button className="user-card-update-btn" type="submit">
+            Save
+          </button>
+        </form>
+      ) : (
+        <LoadingSpinner isLoading={isLoading.toString()} />
+      )}
+    </>
+  );
 }
