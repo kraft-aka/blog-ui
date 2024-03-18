3c6069a4a0c968fc0cb08d0eac16806c87351c82
kubat <kubat.aiupov@gmail.com>
2024-03-18 18:02:33 +0100

    add addReply api

--- src/API/comments.js
+++ src/API/comments.js
@@ -1,11 +1,11 @@
-import axiosInstance from './axiosInstance';
+import axiosInstance from "./axiosInstance";
 
 async function getComments(blogId) {
   try {
     const res = await axiosInstance.get(`comments/${blogId}`);
     return res.data;
   } catch (err) {
-    return false
+    return false;
   }
 }
 
@@ -14,16 +14,18 @@ async function addComment(blogId, newComment) {
     const res = await axiosInstance.post(`blogs/comment/${blogId}`, newComment);
     return res.data;
   } catch (err) {
-    return false
+    return false;
   }
 }
 
 async function deleteComment(commentId) {
   try {
-    const res = await axiosInstance.delete(`comments/deletecomment/${commentId}`);
+    const res = await axiosInstance.delete(
+      `comments/deletecomment/${commentId}`
+    );
     return res.data.msg;
   } catch (err) {
-    return false
+    return false;
   }
 }
 
@@ -35,4 +37,13 @@ async function addLikeToComment(commentId) {
     return false;
   }
 }
-export { getComments, addComment, deleteComment, addLikeToComment };
+
+async function addReply(commentId, reply) {
+  try {
+    const res = await axiosInstance.post(`${commentId}/addreply`, reply);
+    return res.data.savedComment;
+  } catch (err) {
+    return false;
+  }
+}
+export { getComments, addComment, deleteComment, addLikeToComment, addReply };
